fb8f668e7ea760015bc0099fad1ee2060e119fc7
Niv Saar <nivordsar@gmail.com>
2020-06-18 16:37:02 +0300

    section 5

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,102 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="absolute-center">Awesome Todo</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer elevated>
+      <q-tabs>
+        <q-route-tab
+          v-for="(nav,idx) in navs"
+          :key="idx"
+          :to="nav.to"
+          :icon="nav.icon"
+          :label="nav.label"
+        />
+      </q-tabs>
+    </q-footer>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      :breakpoint="767"
+      :width="250"
+      show-if-above
+      bordered
+      content-class="bg-primary"
+    >
+      <q-list dark>
+        <q-item-label header>Navigation</q-item-label>
+        <q-item
+          v-for="(nav,idx) in navs"
+          :key="idx"
+          :to="nav.to"
+          exact
+          clickable
+          class="text-grey-4"
+        >
+          <q-item-section avatar>
+            <q-icon :name="nav.icon"></q-icon>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{nav.label}}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+// import EssentialLink from 'components/EssentialLink'
+
+export default {
+  name: 'MainLayout',
+
+  components: {
+  },
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+      navs: [
+        {
+          label: 'Todo',
+          icon: 'list',
+          to: '/'
+        },
+        {
+          label: 'Settings',
+          icon: 'settings',
+          to: '/settings'
+        },
+        // {
+        //   label: 'About',
+        //   icon: 'info',
+        //   to: '/about'
+        // },
+      ]
+    }
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+@media screen and (min-width: 768px) {
+  .q-footer {
+    display: none;
+  }
+}
+
+.q-drawer {
+  .q-router-link--exact-active {
+    color: white !important;
+  }
+}
+</style>
---
+++ src/pages/PageTodo.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page padding>
+    <p>Todo Page</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
